d443b81c6fac482e0579acdfddf642e95e97feda
Bea Steers <bea.steers@gmail.com>
2026-05-16 12:03:58 -0400

    fix blending

--- src/utils/deckgl/parameters.ts
+++ src/utils/deckgl/parameters.ts
@@ -1,7 +1,7 @@
 import type { DeckRenderParametersOptions } from '../../types';
 
 export const DEFAULT_DECK_PARAMETERS: Required<DeckRenderParametersOptions> = {
-  blend: false,
+  blend: true,
   blendColorOperation: 'add',
   blendColorSrcFactor: 'src-alpha',
   blendColorDstFactor: 'one-minus-src-alpha',
