e46f9468701de466205c7ae57bf5ed4e62604dde
Bea Steers <bea.steers@gmail.com>
2026-05-16 20:50:51 -0400

    move tooltip to utils

--- src/editor/TooltipTemplateEditor.tsx
+++ src/editor/TooltipTemplateEditor.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import type { StandardEditorProps } from '@grafana/data';
 import { HtmlCodeEditor } from './HtmlCodeEditor';
-import { DEFAULT_TOOLTIP_TEMPLATE } from '../extensions/tooltip';
+import { DEFAULT_TOOLTIP_TEMPLATE } from '../utils/tooltip';
 
 export function TooltipTemplateEditor({ value, onChange }: StandardEditorProps<string>) {
   return (
